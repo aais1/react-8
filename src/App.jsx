6048b891a9d3873d7bc9e4544b8ffed4991a2658
AliAais <aaisali228@gmail.com>
2023-11-29 13:51:44 -0800

    visual changes and adding more functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,72 @@
 /* eslint-disable no-unused-vars */
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
 import Addtask from "./components/Addtask.jsx";
 import Tasks from './components/Tasks.jsx';
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ShowAddTask, setShowAddTask] = useState(true);
+  const [Reminder, setReminder] = useState(true);
+  const [Task, setTask] = useState('');
+  const [DateTime, setDayTime] = useState('');
+  const [Alltasks, setAllTasks] = useState([]);
 
-  const [ShowAddTask,setShowAddTask]=useState(true);
-  const [Reminder,setReminder]=useState(true);
-  const [Task,setTask]=useState('');
-  const [DateTime,setDayTime]=useState('');
-
-  const [Alltasks,setAllTasks]=useState([
-    { id: 1, text: 'Example Task 1', day: 'Monday', reminder: false },
-    { id: 2, text: 'Example Task 2', day: 'Tuesday', reminder: true },
-    { id: 3, text: 'Example Task 3 ', day: 'Monday', reminder: false },
-  ]);
-  
-  function handleClick(){
+  function handleClick() {
     setShowAddTask(!ShowAddTask);
   }
 
   function addTask() {
-    const id = Math.floor(Math.random() * 10000) + 1;
+    const id = Math.floor(Math.random() * 100000) + 1;
     if (Task !== '' || DateTime !== '') {
-      const newTask = { id, text: Task, day: DateTime, reminder: Reminder };
+      const newTask = { id, text: Task, day: DateTime, reminder: Reminder, completed: false };
       setAllTasks([newTask, ...Alltasks]);
-      clearInput(); // Move clearInput here
+      clearInput();
     } else {
       alert('Please Enter Task and Day and Time');
     }
   }
-  
 
   function clearInput() {
     setTask('');
     setDayTime('');
   }
-  
-  function removeTask(key){
-    console.log('gi');
-    setAllTasks(Alltasks.filter((task)=>task.id!==key))
+
+  function removeTask(key) {
+    setAllTasks(Alltasks.filter((task) => task.id !== key));
   }
 
+  function completeTask(task) {
+    setAllTasks((prevTasks) =>
+      prevTasks.map((item) =>
+        item.id === task.id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
 
-
-  function OnChangetask(e){
+  function OnChangetask(e) {
     setTask(e.target.value);
   }
 
   return (
     <>
-    <div className="w-[100vw] sm:w-[70vw] md:w-[60vw] m-auto my-[10px]">
-      <div  className='border-blue-100 border-2 p-4 shadow-2xl'>
-      {
-      (ShowAddTask)?
-      <Header onClick={handleClick} color={'red'} text={'Close'}/>
-      :<Header onClick={handleClick} color={'green'} text={'Add'}/>
-      }
+      <div className="w-[100vw] sm:w-[70vw] md:w-[60vw] m-auto my-[10px]">
+        <div className='border-blue-100 border-2 p-4 shadow-2xl'>
+          {ShowAddTask ?
+            <Header onClick={handleClick} color={'red'} text={'Close'} />
+            :
+            <Header onClick={handleClick} color={'green'} text={'Add'} />
+          }
 
-      { ShowAddTask &&
-      <Addtask reminder={Reminder} setReminder={setReminder} setTask={setTask} onclick={addTask}
-       setDayTime={setDayTime} task={Task} dateTime={DateTime} />
-      }
+          {ShowAddTask &&
+            <Addtask reminder={Reminder} setReminder={setReminder} setTask={setTask} onclick={addTask}
+              setDayTime={setDayTime} task={Task} dateTime={DateTime} />
+          }
 
-      <Tasks tasks={Alltasks} removeTask={removeTask}/>
-      
+          <Tasks tasks={Alltasks} removeTask={removeTask} completeTask={completeTask} />
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-function Tasks({tasks,removeTask}) {
+function Tasks({tasks,removeTask,completeTask,Completed}) {
     return (
         <div className='mt-8'>
         {
@@ -7,12 +7,20 @@ function Tasks({tasks,removeTask}) {
                 return(
                     <div key={task.id} className="border-2 p-2 mt-5 flex justify-between">
                         <div className="flex flex-col">
-                            <h3 className="font-bold">{task.text}</h3>
-                            <p>{task.day}</p>
+                            <h3 className={`${task.completed?'line-through opacity-40':''} font-bold`}>{task.text}</h3>
+                            <p className={`${task.completed?'line-through opacity-40':''}`}>{task.day}</p>
                         </div>
                         <div className="flex gap-2">
+
+                            { task.completed ?
                             <button className=" border-2 border-green-500 text-green-500 hover:text-white hover:bg-green-500 font-bold py-1 px-3 rounded-lg duration-150"
-                            onClick={()=>{}}>Edit</button>
+                            onClick={()=>{completeTask(task)}}>Undo</button>
+
+                             : 
+                             
+                             <button className=" border-2 border-green-500 text-green-500 hover:text-white hover:bg-green-500 font-bold py-1 px-5 rounded-lg duration-150"
+                            onClick={()=>{completeTask(task)}}>Complete</button>
+                             }
 
                             <button className="border-2 border-red-500 text-red-500 hover:bg-red-500 hover:text-white font-bold  py-1 px-2 rounded-md duration-150"
                             onClick={()=>{removeTask(task.id)}}>Delete</button>
